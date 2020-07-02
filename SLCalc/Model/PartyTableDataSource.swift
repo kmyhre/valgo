37f834264e7146622b7b1ef09e86a1b798507339
Kristian Myhre <post@kmyhre.net>
2020-07-02 19:25:00 +0200

    MVC compliance

---
+++ SLCalc/Model/PartyTableDataSource.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class PartyTableDataSource: NSObject, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return parties.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        return cell
+    }
+}
