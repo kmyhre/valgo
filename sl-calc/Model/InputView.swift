bff713ac15ae6967fe8c6d43813e944ffc1c8be4
Kristian Myhre <post@kmyhre.net>
2020-07-07 14:24:45 +0200

    Migration to UIView

---
+++ sl-calc/Model/InputView.swift
@@ -0,0 +1,21 @@
+//
+//  InputView.swift
+//  sl-calc
+//
+//  Created by Kristian Myhre on 07/07/2020.
+//  Copyright © 2020 Kristian Myhre. All rights reserved.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
